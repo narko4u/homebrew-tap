223adeebc6ae218fa3861105d244c75509c7cc70
narko4u <narko4u@gmail.com>
2026-07-22 07:19:45 +1000

    fix: update AIP v0.2.0 SHA256 values for all platforms

--- Formula/aip.rb
+++ Formula/aip.rb
@@ -8,10 +8,10 @@ class Aip < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/narko4u/aip-spec/releases/download/v0.2.0/aip_v0.2.0_darwin_arm64.tar.gz"
-      sha256 "UPDATE_AFTER_RELEASE"  # sha256sum aip_v0.2.0_darwin_arm64.tar.gz
+      sha256 "2ea9f2f4a0db9cb654d5c7c88633ba3ae62337f5429fa1eb950153d69160bef6"
     else
       url "https://github.com/narko4u/aip-spec/releases/download/v0.2.0/aip_v0.2.0_darwin_amd64.tar.gz"
-      sha256 "UPDATE_AFTER_RELEASE"  # sha256sum aip_v0.2.0_darwin_amd64.tar.gz
+      sha256 "5d2b927ccef36ef5b78b6e0da215c89e9f3dadc6bb9763a26b238b75dbf28cca"
     end
   end
 
@@ -19,10 +19,10 @@ class Aip < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/narko4u/aip-spec/releases/download/v0.2.0/aip_v0.2.0_linux_arm64.tar.gz"
-      sha256 "UPDATE_AFTER_RELEASE"  # sha256sum aip_v0.2.0_linux_arm64.tar.gz
+      sha256 "884e4f3a59bb2d77f083625e31326161cb4e2734af3a86757cdc79ea34f37b99"
     else
       url "https://github.com/narko4u/aip-spec/releases/download/v0.2.0/aip_v0.2.0_linux_amd64.tar.gz"
-      sha256 "UPDATE_AFTER_RELEASE"  # sha256sum aip_v0.2.0_linux_amd64.tar.gz
+      sha256 "6c6dea41da75b3bcad0daa7991825bdf3985f53cfb0bd88fb8fffa3ab5b0de03"
     end
   end
 
